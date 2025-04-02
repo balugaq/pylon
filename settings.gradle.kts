3d080b79b95de96cb81119f3a23a7dd5e3b7d6f1
Seggan <seggan21@gmail.com>
2025-04-02 15:07:35 -0400

    Remove old code

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,10 +15,7 @@ run {
     }
 }
 
-System.setProperty("io.github.pylonmc.pylon.composite", "true")
-
 plugins {
-    // Apply the foojay-resolver plugin to allow automatic download of JDKs
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
 }
 
